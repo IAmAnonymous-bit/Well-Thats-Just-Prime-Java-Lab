574de87fe3b9cbb79438c21487f18bf349ec6d2d
NNR8608220 <n8reyes2006@gmail.com>
2025-01-28 19:39:02 -0600

    edit

--- app/src/main/java/org/example/Factorizer.java
+++ app/src/main/java/org/example/Factorizer.java
@@ -3,7 +3,7 @@ package org.example;
 import java.util.ArrayList;
 
 public class Factorizer {
-  public ArrayList<Integer> primeFactors(int n) {
+  public ArrayList<Integer> primeFactors(Integer n) {
     // Implement your code here!
     int current = n.intValue();
     int testFactor = 2;
